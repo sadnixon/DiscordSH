c7783e07ab825f06db9a6afeee02c2e176e7f0bb
sadnixon <askspencerhill@berkeley.edu>
2024-05-13 20:29:57 -0700

    creategame formatting

--- commands/creategame.js
+++ commands/creategame.js
@@ -6,42 +6,47 @@ async function execute(message, args, user) {
   console.log(channels);
   if (!(message.channel.id in channels)) {
     const games = await game_info.get("games");
-    const game_id = message.channel.id.toString()+"_"+Date.now().toString();
+    const game_id = message.channel.id.toString() + "_" + Date.now().toString();
     channels[message.channel.id] = game_id;
     games[game_id] = {
-      "customGameSettings": {
-        "deckState": { "lib": 6, "fas": 11 },
-        "trackState": { "lib": 0, "fas": 0 },
-        "powers": [null, "investigate", "election", "bullet", "bullet"],
-        "enabled": false,
-        "hitlerZone": 3,
-        "vetoZone": 5,
-        "fascistCount": 2,
-        "hitKnowsFas": false,
+      customGameSettings: {
+        deckState: { lib: 6, fas: 11 },
+        trackState: { lib: 0, fas: 0 },
+        powers: [null, "investigate", "election", "bullet", "bullet"],
+        enabled: false,
+        hitlerZone: 3,
+        vetoZone: 5,
+        fascistCount: 2,
+        hitKnowsFas: false,
       },
-      "gameSetting": {
-        "rebalance6p": false,
-        "rebalance7p": false,
-        "rebalance9p": false,
-        "casualGame": true,
+      gameSetting: {
+        rebalance6p: false,
+        rebalance7p: false,
+        rebalance9p: false,
+        casualGame: true,
       },
-      "players": [],
-      "logs": [],
-      "gameState": {
-        "lib": 0,
-        "fas": 0,
-        "failedGovs": 0,
-        "deck": shuffleArray(["R", "R", "R", "R", "R", "R", "R", "R", "R", "R", "R",
-        "B","B","B","B","B","B"]),
-        "discard": [],
-        "deadPlayers": [],
-        "hitlerElected": false,
-        "hitlerDead": false,
-      }
+      players: [],
+      logs: [],
+      gameState: {
+        lib: 0,
+        fas: 0,
+        failedGovs: 0,
+        deck: shuffleArray([
+          "R", "R", "R", "R",
+          "R", "R", "R", "R",
+          "R", "R", "R", "B",
+          "B", "B", "B", "B",
+          "B",
+        ]),
+        discard: [],
+        deadPlayers: [],
+        hitlerElected: false,
+        hitlerDead: false,
+      },
     };
     console.log(games[game_id]);
-    await game_info.set("game_channels",channels);
-    await game_info.set("games",games);
+    await game_info.set("game_channels", channels);
+    await game_info.set("games", games);
   }
   //console.log(await game_info.get("game_channels"));
   //console.log(await game_info.get("games"));
